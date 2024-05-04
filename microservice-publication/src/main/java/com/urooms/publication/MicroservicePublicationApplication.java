49d260d7831c91ffb77c71e4df25eda3d954cdcc
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2024-05-04 01:54:32 -0500

    microservice publication base added
    The base of the publication microservice and its initial configuration are uploaded
    
    Co-Authored-By: L.T. <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: MENDOZA BARCO SAUL ENRIQUE <102072719+SaulMendozaB@users.noreply.github.com>
    Co-Authored-By: Jean Carlos Achamizo Huamani <102640048+Sorlac07@users.noreply.github.com>
    Co-Authored-By: Farid <89112952+faridhinostroza@users.noreply.github.com>
    Co-Authored-By: Lucía A. <91093842+luciaaliagat@users.noreply.github.com>

---
+++ microservice-publication/src/main/java/com/urooms/publication/MicroservicePublicationApplication.java
@@ -0,0 +1,15 @@
+package com.urooms.publication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MicroservicePublicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePublicationApplication.class, args);
+	}
+
+}
